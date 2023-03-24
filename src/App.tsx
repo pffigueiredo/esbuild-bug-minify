71cf9d0f09ac4481636b43aa22fe0dea691f47c7
pfigueiredo <klisarkk@gmail.com>
2023-03-24 16:40:58 +0000

    bug with fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+export function setupIconLibrary(consumerCustomIcons: any = []) {
+  library.add(consumerCustomIcons);
+}
 
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <i className='fa-solid fa-user' />
+
+      <i className='fa-regular fa-user' />
+
+      <i className='fa-light fa-user' />
+
+      <i className='fa-duotone fa-user' />
+
+      <i className='fa-thin fa-user' />
+
+      <i className='fa-sharp fa-solid fa-user' />
+
+      <i className='fa-sharp fa-regular fa-user' />
+
+      <i className='fa-brands fa-github-square' />
+    </div>
+  );
 }
